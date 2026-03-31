4b1c4881d3199bfbe6f0f37f07934f8a1b7bc004
Julian <111262742+Marentxs@users.noreply.github.com>
2026-03-30 23:34:59 -0500

    add class ship with constructor, hit method and isSunk method

---
+++ modules/ship.js
@@ -0,0 +1,19 @@
+class Ship {
+  constructor(length) {
+    this.length = length;
+    this.hits = 0;
+    this.sunk = false;
+  }
+
+  hit() {
+    this.hits = this.hits + 1;
+  }
+
+  isSunk() {
+    if (this.hits === this.length) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
