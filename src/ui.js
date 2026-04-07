d32b4c02affe29ba01815da6e364e8415d30ab6c
Julian <111262742+Marentxs@users.noreply.github.com>
2026-04-07 18:09:18 -0500

    add logic for turns in ui module

--- src/ui.js
+++ src/ui.js
@@ -15,18 +15,45 @@ for (let row = 0; row < 10; row++) {
   }
 }
 
+let humanTurn = true;
 for (let row = 0; row < 10; row++) {
   for (let col = 0; col < 10; col++) {
     const btn = document.createElement("button");
     btn.dataset.row = row;
     btn.dataset.col = col;
+
     btn.addEventListener("click", () => {
+      if (!humanTurn) return;
       let result = human.attackClick(computer, row, col);
+
       if (result === "hit" || result === "miss") {
+        opposingBoard
+          .querySelectorAll("button")
+          .forEach((btn) => (btn.disabled = true));
+
+        humanTurn = false;
         lastAttackDiv.textContent = result;
+        btn.style.backgroundColor = "red";
+
+        if (computer.gameboard.allSunk()) {
+          lastAttackDiv.textContent = "Game ended, you won";
+          return;
+        } else {
+          computer.makeRandomAttack(human);
+          if (human.gameboard.allSunk()) {
+            lastAttackDiv.textContent = "Game ended, you lost";
+            return;
+          }
+
+          opposingBoard
+            .querySelectorAll("button")
+            .forEach((btn) => (btn.disabled = false));
+        }
       } else if (result === null) {
         lastAttackDiv.textContent = "Invalid attack, try again";
+        humanTurn = true;
       }
+      humanTurn = true;
     });
     opposingBoard.appendChild(btn);
   }
